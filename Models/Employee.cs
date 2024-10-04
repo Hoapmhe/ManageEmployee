a4d1d959241ae6ecf51b4640b29b2a841004dc9d
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-04 22:35:17 +0700

    add model employee

---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace ManageEmployee.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string FullName { get; set; }
+        public DateTime DOB {  get; set; }
+        public int Age { get; set; }
+        public string Ethnicity { get; set; }
+        public string Job {  get; set; }
+        public string CitizenId { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+}
