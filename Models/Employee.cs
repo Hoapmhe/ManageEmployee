b0873527bf7135b38f88879656e39d70b81164e1
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-07 16:56:14 +0700

    add data annotation

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -1,13 +1,22 @@
-﻿namespace ManageEmployee.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManageEmployee.Models
 {
+    [Table("Employees")]
     public class Employee
     {
-        public int Id { get; set; } 
+        [Key]
+        public int Id { get; set; }
+        [StringLength(255)]
+        [Column(TypeName = "nvarchar")]
+        [Required]
         public string FullName { get; set; }
         public DateTime DOB {  get; set; }
         public int Age { get; set; }
         public string Ethnicity { get; set; }
         public string Job {  get; set; }
+        [Required]
         public string CitizenId { get; set; }
         public string PhoneNumber { get; set; }
 
