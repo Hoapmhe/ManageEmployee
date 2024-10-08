0640f018956f91da60f5c555f53f4bf89dadf498
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-08 11:26:38 +0700

    add DI container

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using ManageEmployee.Data;
+using ManageEmployee.Service;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>
     (option => option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<EmployeeService>();
 
 var app = builder.Build();
 
