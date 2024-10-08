bbede0479ef7afc2a97321f0f3ffd44526611665
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-08 11:28:11 +0700

    employee service

---
+++ Service/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using ManageEmployee.Data;
+using ManageEmployee.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ManageEmployee.Service
+{
+    public class EmployeeService
+    {
+        private readonly AppDbContext _context;
+
+        public EmployeeService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return _context.Employees.ToList();
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            return _context.Employees.FirstOrDefault(e => e.Id == id);
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            _context.Employees.Update(employee);
+            _context.SaveChanges();
+        }
+
+        public bool IsCitizenIdExited(string citizenId)
+        {
+            return _context.Employees.Any(e => e.CitizenId == citizenId);
+        }
+    }
+}
