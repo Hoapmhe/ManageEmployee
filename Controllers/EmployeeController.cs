6ad4f0ca8e57c2a871802ca406918fa1d36ed21d
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-08 10:23:16 +0700

    create new employee

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -17,5 +17,28 @@ namespace ManageEmployee.Controllers
             var listEmployee = _context.Employees.ToList();
             return View(listEmployee);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Employee obj)
+        {
+            bool citizenIdExists = _context.Employees.Any(e => e.CitizenId == obj.CitizenId);
+            if (citizenIdExists)
+            {
+                ModelState.AddModelError("EmployeeError", $"Citizen id '{obj.CitizenId}' has existed");
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Employees.Add(obj);
+                _context.SaveChanges();
+                TempData["Sucess"] = "Create employee sucessesfull";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
