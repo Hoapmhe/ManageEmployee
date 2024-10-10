7c6c17c742a3ad3ace6fcc7808e55109452abd03
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-10 12:37:33 +0700

    model commune

---
+++ Models/Commune.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManageEmployee.Models
+{
+    [Table("Commune")]
+    public class Commune
+    {
+        [Key]
+        public int CommuneId { get; set; }
+        [Required]
+        [StringLength(255)]
+        [Column(TypeName = "nvarchar")]
+        public string CommuneName { get; set; }
+        // Foreign key to District
+        public int DistrictId { get; set; }
+        public District District { get; set; }
+    }
+
+}
