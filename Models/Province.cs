e5cbc1847a21522068905f947888f0df2fdaec99
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-10 12:39:04 +0700

    model province

---
+++ Models/Province.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManageEmployee.Models
+{
+    [Table("Province")]
+    public class Province
+    {
+        [Key]
+        public int ProvinceId { get; set; }
+        [Required]
+        [StringLength(255)]
+        [Column(TypeName = "nvarchar")]
+        public string ProvinceName { get; set; }
+        public ICollection<District> Districts { get; set; }
+
+    }
+}
