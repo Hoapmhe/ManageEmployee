0a918b1dd3f7b0981451e4c018924f06e9ec6f1f
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-11 12:20:35 +0700

    view province

---
+++ Controllers/ProvinceController.cs
@@ -0,0 +1,23 @@
+﻿using ManageEmployee.Models;
+using ManageEmployee.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageEmployee.Controllers
+{
+    public class ProvinceController : Controller
+    {
+        private readonly ProvinceService _provinceService;
+        public ProvinceController(ProvinceService provinceService)
+        {
+            _provinceService = provinceService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var provinces = await _provinceService.GetProvinces();
+            return View(provinces);
+        }
+
+
+
+    }
+}
