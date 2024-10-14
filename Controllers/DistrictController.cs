eb388bdb1b371c98ae877f0a1b3e926613e718a2
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-14 22:47:07 +0700

    create district

--- Controllers/DistrictController.cs
+++ Controllers/DistrictController.cs
@@ -1,19 +1,49 @@
-﻿using ManageEmployee.Service;
+﻿using ManageEmployee.Models;
+using ManageEmployee.Service;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ManageEmployee.Controllers
 {
     public class DistrictController : Controller
     {
         private readonly DistrictService _districtService;
-        public DistrictController(DistrictService districtService)
+        private readonly ProvinceService _provinceService;
+        public DistrictController(DistrictService districtService, ProvinceService provinceService)
         {
             _districtService = districtService;
+            _provinceService = provinceService;
         }
         public async Task<IActionResult> Index()
         {
             var list = await _districtService.GetDistricts();
             return View(list);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var provinceList = await _provinceService.GetProvinces();
+            ViewBag.Provinces = new SelectList(provinceList, "ProvinceId", "ProvinceName"); //Gửi danh sách Province tới view
+            
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Create(District district)
+        {
+            var provinceList = await _provinceService.GetProvinces();
+            ViewBag.Provinces = new SelectList(provinceList, "ProvinceId", "ProvinceName"); // Reassign the provinces for the datalist
+
+            if (district.ProvinceId == 0)
+            {
+                ViewBag.Error = "Please select the province that contains this district.";
+                return View();  // Return the view with validation message
+            }
+
+            _districtService.AddDistrict(district);
+            TempData["Success"] = "Create distric successfully";
+            return RedirectToAction("Index");
         }
     }
 }
--- Models/District.cs
+++ Models/District.cs
@@ -8,15 +8,16 @@ namespace ManageEmployee.Models
     {
         [Key]
         public int DistrictId { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Please enter the district name.")]
         [StringLength(255)]
         [Column(TypeName = "nvarchar")]
         public string DistrictName { get; set; }
 
         // Foreign key to Province
+        [Required]
         public int ProvinceId { get; set; }
         public Province Province { get; set; }
-        public ICollection<Commune> Communes { get; set; }
+        public ICollection<Commune>? Communes { get; set; }
     }
 
 }
