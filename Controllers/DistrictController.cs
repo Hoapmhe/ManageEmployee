509f01f28a75490beae0bb518aac8a44431559bc
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-14 15:58:47 +0700

    display list district

---
+++ Controllers/DistrictController.cs
@@ -0,0 +1,19 @@
+﻿using ManageEmployee.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManageEmployee.Controllers
+{
+    public class DistrictController : Controller
+    {
+        private readonly DistrictService _districtService;
+        public DistrictController(DistrictService districtService)
+        {
+            _districtService = districtService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var list = await _districtService.GetDistricts();
+            return View(list);
+        }
+    }
+}
---
+++ Service/DistrictService.cs
@@ -0,0 +1,38 @@
+﻿using ManageEmployee.Data;
+using ManageEmployee.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ManageEmployee.Service
+{
+    public class DistrictService
+    {
+        private readonly AppDbContext _context;
+        public DistrictService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<District>> GetDistricts()
+        {
+            return await _context.Districts.Include(d => d.Province).ToListAsync();
+        }
+        public bool IsDistrictExisted(string districtName)
+        {
+            return _context.Districts.Any(d => d.DistrictName.Equals(districtName, StringComparison.OrdinalIgnoreCase));
+        }
+        public void AddDistrict(District district)
+        {
+            _context.Districts.Add(district);
+            _context.SaveChanges();
+        }
+        public void UpdateDistrict(District district)
+        {
+            _context.Districts.Update(district);
+            _context.SaveChanges();
+        }
+        public void RemoveDistrict(District district)
+        {
+            _context.Districts.Remove(district);
+            _context.SaveChanges();
+        }
+    }
+}
