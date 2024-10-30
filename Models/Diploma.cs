a3497404667856f328b94b1f84f3b12a4a746f9f
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-30 16:57:59 +0700

    model Diploma

---
+++ Models/Diploma.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ManageEmployee.Models
+{
+    [Table("Diploma")]
+    public class Diploma
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public DateTime IssuedDate { get; set; }
+        public string IssuedByProvince { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public int EmployeeId { get; set; }
+        public Employee Employee { get; set; }
+    }
+}
