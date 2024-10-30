05c7de57ddc94a2e8eb68a3bfbf10556dc4e08de
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-30 17:05:13 +0700

    Diploma Service

---
+++ Service/DiplomaService.cs
@@ -0,0 +1,13 @@
+﻿using ManageEmployee.Data;
+
+namespace ManageEmployee.Service
+{
+    public class DiplomaService
+    {
+        private readonly AppDbContext _context;
+        public DiplomaService(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
