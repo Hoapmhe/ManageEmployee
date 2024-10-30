a99159813aa74182a49e682d8ff46b208f81983b
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-30 17:05:00 +0700

    DiplomaController

---
+++ Controllers/DiplomaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManageEmployee.Controllers
+{
+    public class DiplomaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
