d472621e2ad934470361f9a238057cb7aafd0bab
Phung Minh Hoa <hoaphung10112003@gmail.com>
2024-10-31 21:21:04 +0700

    fix foreign key

--- Models/Diploma.cs
+++ Models/Diploma.cs
@@ -6,6 +6,7 @@ namespace ManageEmployee.Models
     [Table("Diploma")]
     public class Diploma
     {
+        [Key]
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
@@ -13,9 +14,12 @@ namespace ManageEmployee.Models
         public DateTime IssuedDate { get; set; }
         [Required]
         public int IssuedByProvinceId { get; set; }
+        [ForeignKey(nameof(IssuedByProvinceId))]
         public Province IssuedByProvince { get; set; }
         public DateTime? ExpiryDate { get; set; }
+        [Required]
         public int EmployeeId { get; set; }
+        [ForeignKey(nameof(EmployeeId))]
         public Employee Employee { get; set; }
     }
 }
